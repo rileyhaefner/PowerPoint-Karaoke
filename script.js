5e59c0d8437f4f2cdd706e71b5a6e3ec106c3a3c
rileyhaefner <hcps-haefnerrp@henricostudents.org>
2026-04-13 13:53:18 -0400

    Delete script.js
    not needed for project

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  document.getElementById("get_something").addEventListener("click", function(e) {
-    alert("You clicked");
-  });
-
-  document.getElementById("post_something").addEventListener("click", function(e) {
-    alert("You clicked");
-  });
-}
